8fa9cbd0842950911ecfb6e2e32ebac6bd3df7c7
zaim <zaim1.hassani-adili@epitech.eu>
2023-07-18 10:33:42 +0200

    update .gitignore

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL_BACK: "http://217.174.244.115:8080"
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  BASE_URL_BACK:"http://217.174.244.115:8080"
 };
 
 /*
