fc76acde4c2b2aa2ab4e0316d7084e7ccf4b8611
zaim <zaim1.hassani-adili@epitech.eu>
2023-08-26 19:42:13 +0200

    add comment

--- mobile_app/app/src/main/java/com/z_ha/shopmonitor/model/RetrofitSingleton.kt
+++ mobile_app/app/src/main/java/com/z_ha/shopmonitor/model/RetrofitSingleton.kt
@@ -4,6 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitSingleton {
+    //replace the url (217.174.244.115) by the one used in the host server
     val baseUrl = "http://217.174.244.115:8080"
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(baseUrl)
